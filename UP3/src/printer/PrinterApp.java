8d3c4ac95fe540f806367157757f72205c043f4d
Jakub Ptaszkowski <55619673+playfulCloud@users.noreply.github.com>
2023-10-24 00:25:43 +0200

    Update PrinterApp.java

--- UP3/src/printer/PrinterApp.java
+++ UP3/src/printer/PrinterApp.java
@@ -1,15 +1,16 @@
 package printer;
 
 import java.awt.*;
+import java.awt.image.BufferedImage;
 import java.awt.print.*;
 import javax.swing.*;
-import java.awt.image.BufferedImage;
 
 public class PrinterApp implements Printable {
-    private String textToPrint = "Tekst do wydrukowania";
+    private String textToPrint = "Tekst do wydrukowania fajne sobie testuje zajebisty soft do drukarki";
     private Font font = new Font("Arial", Font.PLAIN, 12);
     private Color textColor = Color.BLACK;
     private BufferedImage image = new BufferedImage(100, 100, BufferedImage.TYPE_INT_ARGB);
+    private PageFormat pageFormat = new PageFormat();
 
     @Override
     public int print(Graphics g, PageFormat pf, int pageIndex) {
@@ -18,9 +19,12 @@ public class PrinterApp implements Printable {
         }
 
         Graphics2D g2d = (Graphics2D) g;
-        g2d.translate(pf.getImageableX(), pf.getImageableY());
 
-        // Ustawienie czcionki i koloru tekstu
+        // Ustawienia strony
+        g2d.translate(pf.getImageableX(), pf.getImageableY());
+        g2d.setClip(0, 0, (int) pf.getImageableWidth(), (int) pf.getImageableHeight());
+
+        // Ustawienia czcionki i koloru tekstu
         g2d.setFont(font);
         g2d.setColor(textColor);
 
@@ -45,8 +49,36 @@ public class PrinterApp implements Printable {
         this.image = image;
     }
 
+    public void setPageFormat(PageFormat pageFormat) {
+        this.pageFormat = pageFormat;
+    }
+
     public static void main(String[] args) {
         PrinterApp app = new PrinterApp();
+
+        // Ustawienia strony
+        PageFormat pageFormat = new PageFormat();
+        pageFormat.setOrientation(PageFormat.LANDSCAPE);
+        Paper paper = pageFormat.getPaper();
+        paper.setSize(600, 400); // Rozmiar strony (szerokość, wysokość)
+        paper.setImageableArea(50, 50, 500, 300); // Marginesy (lewy, górny, szerokość, wysokość)
+        pageFormat.setPaper(paper);
+        app.setPageFormat(pageFormat);
+
+        // Ustawienia czcionki
+        Font font = new Font("Times New Roman", Font.BOLD, 16);
+        app.setFont(font);
+
+        // Ustawienia koloru tekstu
+        Color textColor = Color.RED;
+        app.setTextColor(textColor);
+
+        // Ustawienia grafiki
+        BufferedImage image = new BufferedImage(100, 100, BufferedImage.TYPE_INT_ARGB);
+        Graphics2D g2d = image.createGraphics();
+        g2d.setColor(Color.BLUE);
+        g2d.fillRect(0, 0, 100, 100);
+        app.setImage(image);
 
         PrinterJob job = PrinterJob.getPrinterJob();
         job.setPrintable(app);
