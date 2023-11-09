159549ed9e74d302ce7f0c4502c423c5296906d6
Jakub Ptaszkowski <55619673+playfulCloud@users.noreply.github.com>
2023-11-09 12:27:15 +0100

    Update ModemClient.py

--- ModemClient.py
+++ ModemClient.py
@@ -1,32 +1,51 @@
-import time         #unused
-import serial       #gives acess to pySerial library
-import threading    #gives acess to thread creation code
+import serial
+import threading
+from xmodem import XMODEM
 
-
-#Function executed by the listening thread. 
-#It reads byte data coming in from the serial port,
-#translates it if it is able to and sends it to the console
-#Parameter port determines what port object should this function listen to.
-def read_from_port(port):   
+def read_from_port(port):
     while True:
-        data = port.read_until('\r'.encode('utf-8'))
-        try:
-            data = data.decode('utf-8')
-            if data == '':      #Sometimes the modem sends empty data packets, we filter them out.
-                continue
-            print(data)
-        except:
-            continue            #Ignore untranslatable data packets.        
+        data = port.read_until(b'\r')
+        if data:
+            try:
+                print(data.decode('utf-8'), end='')
+            except UnicodeDecodeError:
+                print(data, end='')
 
+def getc(size, timeout=1):
+    return modem_port.read(size) or None
 
-modem_port = serial.Serial(port='COM1', xonxoff=True, dsrdtr=True, timeout=5)   #Create a port object connected to device 'COM1'. 'timeout=5' sets the read timeout to 5 seconds.
+def putc(data, timeout=1):
+    modem_port.write(data)
+    time.sleep(0.1)  # give some time to buffer
+
+# Create a port object connected to device 'COM1'. 'timeout=5' sets the read timeout to 5 seconds.
+modem_port = serial.Serial(port='COM1', xonxoff=True, dsrdtr=True, timeout=5)
 print('Port opened!\n')
-t = threading.Thread(target=read_from_port, daemon=True, args=(modem_port,))    #Create a thread executing the read_from_port() function. Reading from the port blocks the current thread, so it has to be done this way.
-print('Made a listening thread!')
+
+# Start the listening thread
+t = threading.Thread(target=read_from_port, daemon=True, args=(modem_port,))
+print('Starting a listening thread...')
 t.start()
+
+# Initialize XMODEM protocol
+modem = XMODEM(getc, putc)
+
+# Main program loop
 while True:
-    message=input()             #Read the message to be sent from keyboard
-    if message == 'exit':       #If it says 'exit', stop the application
-        exit()
-    else:                       #Otherwise, send it trough the serial port.
-        modem_port.write((message + '\r').encode('utf-8'))  #Note: the commands sent and recieved from the modem have to end with a carriage return symbol ('\r') instead of a newline sumbol ('\n').
+    message = input("Enter command or 'exit': ")
+    
+    if message == 'exit':
+        modem_port.close()
+        break
+    elif message == 'send':
+        filename = input("Enter the filename to send: ")
+        with open(filename, 'rb') as file_to_send:
+            modem.send(file_to_send)
+        print("File sent.")
+    elif message == 'receive':
+        filename = input("Enter the filename to save as: ")
+        with open(filename, 'wb') as file_to_receive:
+            modem.recv(file_to_receive)
+        print("File received.")
+    else:
+        modem_port.write((message + '\r').encode('utf-8'))
